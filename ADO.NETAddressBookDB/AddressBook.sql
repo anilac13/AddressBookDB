01bc4cf9940bd57ce12cb676d217b2dda6d05072
anilac13 <anil4u2496@gmail.com>
2023-06-13 20:11:39 +0530

    [Anil] Add:Create address book table

--- ADO.NETAddressBookDB/AddressBook.sql
+++ ADO.NETAddressBookDB/AddressBook.sql
@@ -1,2 +1,17 @@
 create database AddressBookDB;
 use AddressBookDB;
+
+create table AddressBook
+(
+	Id int primary key identity(1,1),
+	FirstName varchar(50) not null,
+	LastName varchar(50),
+	Address varchar(100),
+	City varchar(50),
+	State varchar(100),
+	Zip varchar(10),
+	PhoneNumber varchar(20),
+	Email varchar(50)
+)
+
+select * from AddressBook
--- ADO.NETAddressBookDB/Program.cs
+++ ADO.NETAddressBookDB/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Data.SqlClient;
+
 namespace ADO.NETAddressBookDB
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Database = AddressBookDB; Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
 
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Insert records to Address book \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        
+                        break;
+                }
+            }
         }
     }
 }
