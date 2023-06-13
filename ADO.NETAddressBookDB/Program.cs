6fcb988911e282c525dc82fe7e37f36f92c0a6e4
anilac13 <anil4u2496@gmail.com>
2023-06-13 20:28:53 +0530

    [Anil] Add:Insert new contact to the address book

--- ADO.NETAddressBookDB/Program.cs
+++ ADO.NETAddressBookDB/Program.cs
@@ -18,7 +18,31 @@ namespace ADO.NETAddressBookDB
                 switch (option)
                 {
                     case 1:
-                        
+                        connection.Open();
+                        Console.WriteLine("Enter first name");
+                        string first = Console.ReadLine();
+                        Console.WriteLine("Enter last name");
+                        string last = Console.ReadLine();
+                        Console.WriteLine("Enter address");
+                        string address = Console.ReadLine();
+                        Console.WriteLine("Enter city");
+                        string city = Console.ReadLine();
+                        Console.WriteLine("Enter state");
+                        string state = Console.ReadLine();
+                        Console.WriteLine("Enter zip code");
+                        string zip = Console.ReadLine();
+                        Console.WriteLine("Enter phone number");
+                        string phone = Console.ReadLine();
+                        Console.WriteLine("Enter email");
+                        string email = Console.ReadLine();
+
+                        string query = $"insert into AddressBook values('{first}','{last}','{address}','{city}','{state}','{zip}','{phone}','{email}')";
+                        SqlCommand insertCommand = new SqlCommand(query, connection);
+                        insertCommand.ExecuteNonQuery();
+                        connection.Close();
+                        break;
+                    case 2:
+                        flag = false;
                         break;
                 }
             }
