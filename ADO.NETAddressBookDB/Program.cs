94861fbc2e6d875fef15974cc48037b13635f6a7
anilac13 <anil4u2496@gmail.com>
2023-06-13 20:09:14 +0530

    [Anil] Add:Create address book table

--- ADO.NETAddressBookDB/Program.cs
+++ ADO.NETAddressBookDB/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Data.SqlClient;
+
 namespace ADO.NETAddressBookDB
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Database = AddressBookDB; Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
         }
     }
 }
